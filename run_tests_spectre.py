ddf1d5150f11756268c3aad793bd349f6c00054d
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2018-06-26 14:43:19 +0200

    run_tests_spectre now supports nested directories in benchmark directory

--- run_tests_spectre.py
+++ run_tests_spectre.py
@@ -5,9 +5,11 @@ spectre_exec = "/Users/bernhard/repos/spectre/build_xcode/bin/Debug/spectre"
 vampire_exec = "/Users/bernhard/repos/vampire/build_xcode/bin/Debug/vampire"
 example_dir = "/Users/bernhard/repos/spectre/examples"
 
+spectre_standard_args = ""
+vampire_standard_args = "--avatar off --input_syntax smtlib2"
+
 # test function and return whether test has succeeded
-def process(filename, timelimitSeconds, spectreArgs, vampireArgs):
-	filepath = example_dir + "/" + filename
+def process(filepath, filename, timelimitSeconds, spectreArgs, vampireArgs):
 
 	spectre_out_file_path = "./spectre_out/" + filename + ".tptp"
 	vampire_out_file_path = "./vampire_out/" + filename + ".vout"
@@ -16,7 +18,7 @@ def process(filename, timelimitSeconds, spectreArgs, vampireArgs):
 	os.system(spectre_exec + " " + spectreArgs + " " + filepath + " > " + spectre_out_file_path)
 
 	# run Vampire on spectre_out_file_path with given timelimit and save result in vampire_out_file_path
-	os.system(vampire_exec + " --avatar off --input_syntax smtlib2 " + vampireArgs + " -t " + str(timelimitSeconds) + "s " + spectre_out_file_path + " > " + vampire_out_file_path)
+	os.system(vampire_exec + " " + vampireArgs + " -t " + str(timelimitSeconds) + "s " + spectre_out_file_path + " > " + vampire_out_file_path)
 
 	# check whether proof was found and update stats
 	if '% Refutation found. Thanks to Tanya!' in open(vampire_out_file_path, 'r').read():
@@ -28,49 +30,54 @@ def process(filename, timelimitSeconds, spectreArgs, vampireArgs):
 
 def runAllTests(timelimitSeconds, spectreArgs, vampireArgs):
 	print("Running all benchmarks for " + str(timelimitSeconds) + " seconds")
-	if spectreArgs != "":
-		print("Additional Spectre arguments: " + spectreArgs)
-	if vampireArgs != "":
-		print("Additional Vampire arguments: " + vampireArgs)
+	print("Spectre arguments: " + spectreArgs)
+	print("Vampire arguments: " + vampireArgs)
 
 	success = 0
 	fail = 0
 
-	# test each file in example_dir and print stats
-	for filename in sorted(os.listdir(example_dir)):
-	    result = process(filename, timelimitSeconds, spectreArgs, vampireArgs)
-	    if result == True:
-	    	success += 1
-	    else:
-	    	fail += 1
+	# test each file in example_dir(recursively) and print stats
+	for root, directories, filenames in os.walk(example_dir):
+		print("\nScanning folder " + root)
 
-	print("\nSPECTRE: Test results:")
+		for filename in sorted(filenames):
+			if filename == ".DS_Store":
+				continue
+			filepath = os.path.join(root,filename)
+
+			result = process(filepath, filename, timelimitSeconds, spectreArgs, vampireArgs)
+			if result == True:
+				success += 1
+			else:
+				fail += 1
+
+	print("\nSPECTRE: Overall Test results:")
+	print("Running all benchmarks for " + str(timelimitSeconds) + " seconds")
+	print("Spectre arguments: " + spectreArgs)
+	print("Vampire arguments: " + vampireArgs)
 	print("#success:" + str(success))
 	print("#fail:" + str(fail))
 
 def runSingleTest(filename, timelimitSeconds, spectreArgs, vampireArgs):
 	print("Running benchmark " + filename + " for " + str(timelimitSeconds) + " seconds")
-	if spectreArgs != "":
-		print("Additional Spectre arguments: " + spectreArgs)
-	if vampireArgs != "":
-		print("Additional Vampire arguments: " + vampireArgs)
+	print("Spectre arguments: " + spectreArgs)
+	print("Vampire arguments: " + vampireArgs)
 		
-	result = process(filename, timelimitSeconds, spectreArgs, vampireArgs)
-
-	print("\nSPECTRE: Test result for file " + filename + " :")
-	if result == True:
-		print("success!")
-	else:
-		print("failed!")
+	filepath = os.path.join(example_dir, filename)
+	print(filepath)
+	result = process(filepath, filename, timelimitSeconds, spectreArgs, vampireArgs)
 
 parser = argparse.ArgumentParser(description='Run spectre tests')
 parser.add_argument("-f", "--single-file", required=False, default="", action="store", dest="name", help="run tests only on file from example-dir with given name")
 parser.add_argument("-t", "--time", required=False, default=5, action="store", dest="time", help="set time-limit (seconds) for Vampire (default: 5)")
-parser.add_argument("-sargs", "--spectre-args", required=False, default="", action="store", dest="sargs", help="additional arguments for Spectre")
-parser.add_argument("-vargs", "--vampire-args", required=False, default="", action="store", dest="vargs", help="additional arguments for Vampire")
+parser.add_argument("-sargs", "--spectre-args", required=False, default="", action="store", dest="additionalSpectreArgs", help="additional arguments for Spectre")
+parser.add_argument("-vargs", "--vampire-args", required=False, default="", action="store", dest="additionalVampireArgs", help="additional arguments for Vampire")
 args = parser.parse_args()
 
+spectreArgs = " " + spectre_standard_args + " " + args.additionalSpectreArgs + " "
+vampireArgs = " " + vampire_standard_args + " " + args.additionalVampireArgs + " "
+
 if args.name != "":
-	runSingleTest(args.name,args.time,args.sargs,args.vargs)
+	runSingleTest(args.name,args.time,spectreArgs, vampireArgs)
 else:
-	runAllTests(args.time,args.sargs,args.vargs)
+	runAllTests(args.time, spectreArgs, vampireArgs)
